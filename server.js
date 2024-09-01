a5e39a3b2f3076af35cce4a0f9452690fa90eaf0
Lucas Fidelis <lucasm241301@gmail.com>
2024-09-01 19:31:53 -0300

    Ajusta tabela de eventos e atualiza schema

--- server.js
+++ server.js
@@ -12,49 +12,63 @@ const db = new sqlite3.Database('eventos.db', sqlite3.OPEN_READWRITE | sqlite3.O
     console.log('Conectado ao banco de dados.')
 })
 
+db.run('drop table eventos')
 db.run(`
     CREATE TABLE IF NOT EXISTS eventos (
         id_evento UUID,
         titulo VARCHAR(45) NOT NULL,
-        rua VARCHAR(45) NOT NULL,
-        numero VARCHAR(45) NOT NULL,
+        rua VARCHAR(120) NOT NULL,
+        numero VARCHAR(8) NOT NULL,
         bairro VARCHAR(20) NOT NULL,
-        complemento VARCHAR(45),
-        data_inicio DATE NOT NULL,
-        horario TIMESTAMP NOT NULL,
+        complemento VARCHAR(30),
+        data_inicio CHAR(10) NOT NULL,
+        horario CHAR(5) NOT NULL,
         CONSTRAINT pk_id_evento PRIMARY KEY (id_evento)
     )
 `)
-// db.run('drop table eventos')
 
 const schemaEvento = Joi.object({
-    title: Joi.string().min(3).required().messages({
+    title: Joi.string().min(3).max(45).required().messages({
         'string.base': ('Titulo deve ser uma string').toLowerCase(),
-        'string.empty': ('Titulo não pode estar vazio').toLowerCase()
+        'string.empty': ('Titulo não pode estar vazio').toLowerCase(),
+        'string.min': ('Titulo deve conter no mínimo 3 caracteres').toLowerCase(),
+        'string.max': ('Titulo deve conter no máximo 45 caracteres').toLowerCase()
+
     }),
     endereco: Joi.object({
-        rua: Joi.string().required().messages({
+        rua: Joi.string().min(10).max(120).required().messages({
             'string.base': ('Rua deve ser uma string').toLowerCase(),
-            'string.empty': ('Rua não pode estar vazia').toLowerCase()
+            'string.empty': ('Rua não pode estar vazia').toLowerCase(),
+            'string.min': ('Rua deve conter no mínimo 10 caracteres').toLowerCase(),
+            'string.max': ('Rua deve conter no máximo 120 caracteres').toLowerCase()
         }),
-        numero: Joi.string().pattern(new RegExp('^[a-zA-Z0-9\s]+$')).required().messages({
+        numero: Joi.string().max(8).pattern(new RegExp('^[a-zA-Z0-9\s]+$')).required().messages({
             'string.base': ('Número deve ser uma string').toLowerCase(),
             'string.empty': ('Número não pode estar vazio').toLowerCase(),
-            'string.pattern.base': ('Número deve conter apenas caracteres alfanuméricos').toLowerCase()
+            'string.pattern.base': ('Número deve conter apenas caracteres alfanuméricos').toLowerCase(),
+            'string.max': ('Número deve conter no máximo 8 caracteres').toLowerCase()
         }),
-        bairro: Joi.string().required().messages({
+        bairro: Joi.string().min(5).max(20).required().messages({
             'string.base': ('Bairro deve ser uma string').toLowerCase(),
-            'string.empty': ('Bairro não pode estar vazio').toLowerCase()
+            'string.empty': ('Bairro não pode estar vazio').toLowerCase(),
+            'string.min': ('Bairro deve conter no mínimo 5 caracteres').toLowerCase(),
+            'string.max': ('Bairro deve conter no máximo 20 caracteres').toLowerCase()
+        }),
+        complemento: Joi.string().max(30).messages({
+            'string.base': ('Complemento deve ser uma string').toLowerCase(),
+            'string.max': ('Complemento deve conter no máximo 30 caracteres').toLowerCase()
         }),
     }).required(),
-    data: Joi.string().pattern(new RegExp('^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$')).required().messages({
+    data: Joi.string().length(10).pattern(new RegExp('^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$')).required().messages({
         'string.base': ('Data deve ser uma string').toLowerCase(),
         'string.empty': ('Data não pode estar vazia').toLowerCase(),
+        'string.length': ('Data deve conter 10 caracteres').toLowerCase(),
         'string.pattern.base': ('Data deve estar no modelo (dd/mm/yyyy)').toLowerCase()
     }),
-    horario: Joi.string().pattern(new RegExp('^([01][0-9]|2[0-3]):([0-5][0-9])$')).required().messages({
+    horario: Joi.string().length(5).pattern(new RegExp('^([01][0-9]|2[0-3]):([0-5][0-9])$')).required().messages({
         'string.base': ('Horário deve ser uma string').toLowerCase(),
         'string.empty': ('Horário não pode estar vazio').toLowerCase(),
+        'string.length': ('Horário deve conter 5 caracteres').toLowerCase(),
         'string.pattern.base': ('Horário deve estar no modelo (hh:mm)').toLowerCase()
     })
 });
@@ -103,7 +117,7 @@ server.get('/eventos/:id', (request, reply) => {
         }
         if (!row) {
             return reply.status(404).send({ message: 'Evento não encontrado' })
-        } 
+        }
 
         return reply.status(200).send(row)
     })
