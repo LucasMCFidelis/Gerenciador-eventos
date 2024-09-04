4d3105885c46f99b35e3a84b0020bb84a96ee091
Lucas Fidelis <lucasm241301@gmail.com>
2024-09-04 08:48:27 -0300

    Acrescenta validação para evitar cadastramento de campos apenas com espaços em branco

--- schemas/schemaEvento.js
+++ schemas/schemaEvento.js
@@ -1,33 +1,51 @@
 import Joi from "joi"
 
+const validarApenasEspacos = (value, helpers, message) => {
+    value = value.trim()
+    if (value === '') {
+        return helpers.message(message.toLowerCase())
+    }
+    return value
+}
+
 export const schemaEvento = Joi.object({
-    title: Joi.string().min(3).max(45).required().messages({
+    title: Joi.string().min(3).max(120).required().custom(
+        (value, helpers) => validarApenasEspacos(value, helpers, 'Título não pode conter apenas espaços em branco')
+    ).messages({
         'string.base': ('Titulo deve ser uma string').toLowerCase(),
         'string.empty': ('Titulo não pode estar vazio').toLowerCase(),
         'string.min': ('Titulo deve conter no mínimo 3 caracteres').toLowerCase(),
-        'string.max': ('Titulo deve conter no máximo 45 caracteres').toLowerCase()
+        'string.max': ('Titulo deve conter no máximo 120 caracteres').toLowerCase()
 
     }),
     endereco: Joi.object({
-        rua: Joi.string().min(10).max(120).required().messages({
+        rua: Joi.string().min(10).max(120).required().custom(
+            (value, helpers) => validarApenasEspacos(value, helpers, 'Rua não pode conter apenas espaços em branco')
+        ).messages({
             'string.base': ('Rua deve ser uma string').toLowerCase(),
             'string.empty': ('Rua não pode estar vazia').toLowerCase(),
             'string.min': ('Rua deve conter no mínimo 10 caracteres').toLowerCase(),
             'string.max': ('Rua deve conter no máximo 120 caracteres').toLowerCase()
         }),
-        numero: Joi.string().max(8).pattern(new RegExp('^[a-zA-Z0-9\s]+$')).required().messages({
+        numero: Joi.string().max(8).pattern(new RegExp('^[a-zA-Z0-9\s]+$')).required().custom(
+            (value, helpers) => validarApenasEspacos(value, helpers, 'Número não pode conter apenas espaços em branco')
+        ).messages({
             'string.base': ('Número deve ser uma string').toLowerCase(),
             'string.empty': ('Número não pode estar vazio').toLowerCase(),
             'string.pattern.base': ('Número deve conter apenas caracteres alfanuméricos').toLowerCase(),
             'string.max': ('Número deve conter no máximo 8 caracteres').toLowerCase()
         }),
-        bairro: Joi.string().min(5).max(20).required().messages({
+        bairro: Joi.string().min(5).max(20).required().custom(
+            (value, helpers) => validarApenasEspacos(value, helpers, 'Bairro não pode conter apenas espaços em branco')
+        ).messages({
             'string.base': ('Bairro deve ser uma string').toLowerCase(),
             'string.empty': ('Bairro não pode estar vazio').toLowerCase(),
             'string.min': ('Bairro deve conter no mínimo 5 caracteres').toLowerCase(),
             'string.max': ('Bairro deve conter no máximo 20 caracteres').toLowerCase()
         }),
-        complemento: Joi.string().max(30).messages({
+        complemento: Joi.string().max(30).optional().custom(
+            (value, helpers) => validarApenasEspacos(value, helpers, 'Complemento não pode conter apenas espaços em branco')
+        ).messages({
             'string.base': ('Complemento deve ser uma string').toLowerCase(),
             'string.max': ('Complemento deve conter no máximo 30 caracteres').toLowerCase()
         }),
