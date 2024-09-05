bc82ba047309b03a0273e493fa481a1852dfbf3d
Lucas Fidelis <lucasm241301@gmail.com>
2024-09-05 20:50:46 -0300

    Atualiza importação do banco de dados

--- eventos.js
+++ eventos.js
@@ -1,4 +1,4 @@
-import { db } from "./server.js"
+import { db } from "./createDatabase.js"
 import { schemaEvento } from "./schemas/schemaEvento.js"
 import { randomUUID } from "crypto"
 
@@ -46,46 +46,6 @@ export class Eventos {
         } catch (error) {
             return reply.status(400).send({
                 error: "Erro de validação",
-                details: error.details.map(detail => detail.message)
-            })
-        }
-    }
-
-    async update(request, reply) {
-        try {
-            const eventoId = request.params.id;
-    
-            await db.get('SELECT id_evento FROM eventos WHERE id_evento = ?', [eventoId], async (error, row) => {
-                if (error) {
-                    console.error(error.message)
-                    return reply.status(500).send({ message: 'Erro ao consultar o evento' })
-                }
-    
-                if (!row) {
-                    return reply.status(404).send({ message: 'Evento não encontrado' })
-                }
-    
-                const value = await schemaEvento.validateAsync(request.body)
-                const { title, endereco, data, horario } = value;
-                const { rua, numero, bairro } = endereco;
-                await db.run(`
-                    UPDATE eventos 
-                    SET titulo = ?, rua = ?, numero = ?, bairro = ?, data_inicio = ?, horario = ?
-                    WHERE id_evento = ?
-                    `, [title, rua, numero, bairro, data, horario, eventoId], (error) => {
-                    if (error) {
-                        console.error(error.message)
-                        return reply.status(500).send({ message: 'Erro ao deletar evento' })
-                    }
-                })
-    
-                reply.status(204).send()
-            })
-    
-        } catch (error) {
-            return reply.status(400).send({
-                error: 'Erro de validação',
-                code: 400,
                 details: error.details.map(detail => detail.message)
             })
         }
