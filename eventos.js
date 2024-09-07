46609bd5336e60ce89f8e37612b3e687fee35a81
Lucas Fidelis <lucasm241301@gmail.com>
2024-09-06 22:25:59 -0300

    Retora funcionalidade de update de eventos

--- eventos.js
+++ eventos.js
@@ -16,7 +16,7 @@ export class Eventos {
             }
         })
     }
-    
+
     async get(eventoId, reply) {
         await db.get('SELECT * FROM eventos WHERE id_evento = ?', [eventoId], (error, row) => {
             if (error) {
@@ -26,11 +26,11 @@ export class Eventos {
             if (!row) {
                 return reply.status(404).send({ message: 'Evento não encontrado' })
             }
-    
+
             return reply.status(200).send(row)
         })
     }
-    
+
     async create(request, reply) {
         try {
             const value = await schemaEvento.validateAsync(request.body)
@@ -51,24 +51,63 @@ export class Eventos {
         }
     }
 
+    async update(request, reply) {
+        try {
+            const eventoId = request.params.id;
+
+            await db.get('SELECT id_evento FROM eventos WHERE id_evento = ?', [eventoId], async (error, row) => {
+                if (error) {
+                    console.error(error.message)
+                    return reply.status(500).send({ message: 'Erro ao consultar o evento' })
+                }
+
+                if (!row) {
+                    return reply.status(404).send({ message: 'Evento não encontrado' })
+                }
+
+                const value = await schemaEvento.validateAsync(request.body)
+                const { title, endereco, data, horario } = value;
+                const { rua, numero, bairro } = endereco;
+                await db.run(`
+                    UPDATE eventos 
+                    SET titulo = ?, rua = ?, numero = ?, bairro = ?, data_inicio = ?, horario = ?
+                    WHERE id_evento = ?
+                    `, [title, rua, numero, bairro, data, horario, eventoId], (error) => {
+                    if (error) {
+                        console.error(error.message)
+                        return reply.status(500).send({ message: 'Erro ao deletar evento' })
+                    }
+                })
+
+                reply.status(204).send()
+            })
+
+        } catch (error) {
+            return reply.status(400).send({
+                error: 'Erro de validação',
+                details: error.details.map(detail => detail.message)
+            })
+        }
+    }
+
     async delete(eventoId, reply) {
         await db.get('SELECT id_evento FROM eventos WHERE id_evento = ?', [eventoId], async (error, row) => {
             if (error) {
                 console.error(error.message)
                 return reply.status(500).send({ message: 'Erro ao consultar o evento' })
             }
-    
+
             if (!row) {
                 return reply.status(404).send({ message: 'Evento não encontrado' })
             }
-    
+
             await db.run('DELETE FROM eventos WHERE id_evento = ?', [eventoId], (error) => {
                 if (error) {
                     console.error(error.message)
                     return reply.status(500).send({ message: 'Erro ao deletar evento' })
                 }
             })
-    
+
             reply.status(204).send()
         })
     }
