7b64f18125e3758b9ad0bcac9b30db62883c11a3
Lucas Fidelis <lucasm241301@gmail.com>
2024-09-18 17:21:55 -0300

    Ajusta handleError para tipar os parametros que são esperados

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -1,9 +1,12 @@
-export function handleError(error, reply) {
+import { FastifyReply } from "fastify"
+
+export function handleError(error: Error, reply: FastifyReply) {
     console.error(error.message)
-    if (error.details) {
+
+    if (error.message) {
         return reply.status(400).send({
             error: 'Erro de validação',
-            details: error.details.map(detail => (detail.message).toLowerCase())
+            message: error.message.toLowerCase()
         })
     } else {
         return reply.status(500).send({ message: 'Erro interno ao realizar operação' })
