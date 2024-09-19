9c2f2599468eb2224b83b2cffe08e803ab59d0bb
Lucas Fidelis <lucasm241301@gmail.com>
2024-09-19 14:31:06 -0300

    Ajusta handleError para receber error como desconhecido, e lançar um erro de validação caso o parametro seja uma instancia de Error

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -1,14 +1,16 @@
-import { FastifyReply } from "fastify"
+import { FastifyReply } from "fastify";
 
-export function handleError(error: Error, reply: FastifyReply) {
-    console.error(error.message)
-
-    if (error.message) {
+export function handleError(error: unknown, reply: FastifyReply) {
+    // Verifica se o erro é uma instância de Error
+    if (error instanceof Error) {
+        console.error(error.message)
         return reply.status(400).send({
             error: 'Erro de validação',
             message: error.message.toLowerCase()
         })
     } else {
+        // Para erros não previstos ou que não sejam do tipo Error
+        console.error('Erro desconhecido', error)
         return reply.status(500).send({ message: 'Erro interno ao realizar operação' })
     }
 }
