9f1ef423e2b6fec09492b841732cc8495fd5da5a
Lucas Fidelis <lucasm241301@gmail.com>
2024-09-25 14:45:01 -0300

    Austa rota de busca de evento por id para usar prisma ORM

--- src/http/eventos.ts
+++ src/http/eventos.ts
@@ -65,18 +65,20 @@ export async function eventos(fastify: FastifyInstance) {
         })
     })
 
-    fastify.get('/eventos/:id', (request, reply) => {
-        const eventoId = (request.params as { id: string }).id
-        db.get('SELECT * FROM eventos WHERE id_evento = ?', [eventoId], (error, row) => {
-            if (error) {
-                console.error(error.message)
-                return reply.status(500).send({ message: 'Erro ao consultar o evento' })
+    fastify.get('/eventos/:id', async (request, reply) => {
+        const eventId = (request.params as { id: string }).id
+        await prisma.event.findUnique({
+            where: {
+                eventId
             }
-            if (!row) {
+        }).then((event) => {
+            if (!event) {
                 return reply.status(404).send({ message: 'Evento não encontrado' })
             }
-
-            return reply.status(200).send(row)
+            return reply.status(200).send(event)
+        }).catch((error) => {
+            console.error(error)
+            return reply.status(500).send({ message: 'Erro ao consultar o evento' })
         })
     })
 
