30842ab05272609f530963c5f5c7cb6197564838
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-02 08:52:38 -0300

    Cria um arquivo separado para a rota put de atualização dos dados basicos de um usuário

---
+++ src/http/users/updateUserRoute.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance } from "fastify";
+import { checkExistingUser } from "../../utils/checkExistingUser.js";
+import { schemaCadastre } from "../../schemas/schemaCadastre.js";
+import { getUserById } from "../../utils/getUserById.js";
+import { handleError } from "../../utils/handleError.js";
+import { prisma } from "../../utils/prisma.js";
+
+export async function updateUserRoute(fastify:FastifyInstance) {
+    fastify.put('/usuarios/id/:id', async (request, reply) => {
+        try {
+            const userId = (request.params as { id: string }).id
+            const user = await getUserById(userId)
+            if (!user) {
+                return reply.status(404).send({ message: 'Usuário não encontrado' })
+            }
+
+            const userDataValidate = await schemaCadastre.validateAsync(request.body)
+            const { firstName, lastName, email, phoneNumber } = userDataValidate
+            
+            const {status, existingUser, message} = await checkExistingUser(email)           
+            if (existingUser) {
+                return reply.status(status).send({message})
+            }
+
+            await prisma.user.update({
+                where: {
+                    userId: user.userId
+                },
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    phoneNumber
+                }
+            }).then(() => {
+                return reply.status(200).send({ message: 'Usuário atualizado com sucesso' })
+            })
+        } catch (error) {
+            return handleError(error, reply)
+        }
+    })
+}
--- src/http/usuarios.ts
+++ src/http/usuarios.ts
@@ -16,40 +16,6 @@ interface UserAutentication {
 }
 
 export async function usuarios(fastify: FastifyInstance) {
-    fastify.put('/usuarios/id/:id', async (request, reply) => {
-        try {
-            const userId = (request.params as { id: string }).id
-            const user = await getUserById(userId)
-            if (!user) {
-                return reply.status(404).send({ message: 'Usuário não encontrado' })
-            }
-
-            const userDataValidate = await schemaCadastre.validateAsync(request.body)
-            const { firstName, lastName, email, phoneNumber } = userDataValidate
-            
-            const {status, existingUser, message} = await checkExistingUser(email)           
-            if (existingUser) {
-                return reply.status(status).send({message})
-            }
-
-            await prisma.user.update({
-                where: {
-                    userId: user.userId
-                },
-                data: {
-                    firstName,
-                    lastName,
-                    email,
-                    phoneNumber
-                }
-            }).then(() => {
-                return reply.status(200).send({ message: 'Usuário atualizado com sucesso' })
-            })
-        } catch (error) {
-            return handleError(error, reply)
-        }
-    })
-
     fastify.delete('/usuarios/id/:id', async (request, reply) => {
         try {
             const userId = (request.params as { id: string }).id
