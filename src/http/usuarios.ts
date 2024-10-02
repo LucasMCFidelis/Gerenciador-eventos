e48e76527b747f1416cff59b00b86c95e6f35fcf
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-02 08:51:17 -0300

    Cria um arquivo separado para a rota post de criação dos usuários

--- src/http/usuarios.ts
+++ src/http/usuarios.ts
@@ -1,23 +1,13 @@
-import bcrypt from "bcrypt"
 import { schemaCadastre } from "../schemas/schemaCadastre.js"
 import { schemaUserPassword } from "../schemas/schemaUserPassword.js"
 import { handleError } from "../utils/handleError.js"
-import { FastifyInstance, FastifyReply } from "fastify"
+import { FastifyInstance } from "fastify"
 import { prisma } from "../utils/prisma.js"
 import { checkExistingUser } from "../utils/checkExistingUser.js"
 import { getUserById } from "../utils/getUserById.js"
 import { getUserByEmail } from "../utils/getUserByEmail.js"
-import { hashPassword } from "../utils/hashPassword.js"
 import { comparePasswords } from "../utils/comparePasswords.js"
 import { updateUserPassword } from "../utils/updateUserPassword.js"
-
-interface Cadastro {
-    firstName: string
-    lastName: string
-    email: string
-    phoneNumber?: string | null
-    password: string
-}
 
 interface UserAutentication {
     userId: string
@@ -26,51 +16,6 @@ interface UserAutentication {
 }
 
 export async function usuarios(fastify: FastifyInstance) {
-    fastify.post('/usuarios', async (request, reply) => {
-        try {
-            const { firstName, lastName, email, phoneNumber, password } = request.body as Cadastro
-            await schemaCadastre.validateAsync({
-                firstName,
-                lastName,
-                email,
-                phoneNumber
-            })
-            await schemaUserPassword.validateAsync({
-                password
-            })
-            const senhaHash = await hashPassword(password)
-            
-            const {status, existingUser, message} = await checkExistingUser(email)           
-            if (existingUser) {
-                return reply.status(status).send({message})
-            }
-
-            await prisma.user.create({
-                data: {
-                    firstName,
-                    lastName,
-                    email,
-                    phoneNumber,
-                    password: senhaHash
-                }
-            }).then((usuario) => {
-                return reply.status(200).send({
-                    userId: usuario.userId,
-                    firstName: usuario.firstName,
-                    lastName: usuario.lastName,
-                    email: usuario.email,
-                    phoneNumber: usuario.phoneNumber,
-                })
-            }).catch((error) => {
-                console.error(error)
-                return reply.status(500).send({ message: 'Erro ao salvar cadastro' })
-            })
-
-        } catch (error) {
-            return handleError(error, reply)
-        }
-    })
-
     fastify.put('/usuarios/id/:id', async (request, reply) => {
         try {
             const userId = (request.params as { id: string }).id
