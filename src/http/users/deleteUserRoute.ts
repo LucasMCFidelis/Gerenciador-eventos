81f07d3764f68e9526ae9b0331a82f2d818a1f1a
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-04 13:56:24 -0300

    Ajusta rotas deleteUserRoute e getUserRoute

--- src/http/users/deleteUserRoute.ts
+++ src/http/users/deleteUserRoute.ts
@@ -1,25 +1,34 @@
-import { FastifyInstance } from "fastify";
-import { getUserById } from "../../utils/db/getUserById.js";
-import { handleError } from "../../utils/handlers/handleError.js";
-import { prisma } from "../../utils/db/prisma.js";
+import { FastifyInstance } from "fastify"
+import { getUserById } from "../../utils/db/getUserById.js"
+import { handleError } from "../../utils/handlers/handleError.js"
+import { prisma } from "../../utils/db/prisma.js"
 
-export async function deleteUserRoute(fastify:FastifyInstance) {
+export async function deleteUserRoute(fastify: FastifyInstance) {
     fastify.delete('/usuarios/id/:id', async (request, reply) => {
         try {
+            // 1. Verifica se o parâmetro id foi fornecido
             const userId = (request.params as { id: string }).id
-            const user = await getUserById(userId)
-            if (!user) {
-                return reply.status(404).send({ message: 'Usuário não encontrado' })
+            if (!userId) {
+                return reply.status(400).send({ message: 'ID deve ser fornecido' })
             }
 
+            // 2. Verifica se o usuário existe
+            const userResponse = await getUserById(userId)
+            if (!userResponse.data || userResponse.error) {
+                return reply.status(userResponse.status).send({ message: userResponse.message })
+            }
+
+            // 3. Deletar usuário 
             await prisma.user.delete({
                 where: {
                     userId
                 }
-            }).then(() => {
-                reply.status(204).send()
             })
+
+            // 4. Sucesso ao deletar usuário
+            return reply.status(204).send()
         } catch (error) {
+            // 5. Tratamento de erros genéricos
             return handleError(error, reply)
         }
     })
--- src/http/users/getUserRoute.ts
+++ src/http/users/getUserRoute.ts
@@ -6,7 +6,7 @@ export async function getUserRoute(fastify: FastifyInstance) {
     fastify.get('/usuarios/id/:id', async (request, reply) => {
         try {
             // 1. Verifica se o parâmetro id foi fornecido
-            const { id: userId } = request.params as { id: string }
+            const userId = (request.params as { id: string }).id
             if (!userId) {
                 return reply.status(400).send({ message: 'ID deve ser fornecido' })
             }
