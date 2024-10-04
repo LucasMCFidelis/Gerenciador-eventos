6ba50c34a14473269b5331acd4ee91b0065b8907
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-04 09:51:32 -0300

    Ajusta rota updateUserRoute

--- src/http/users/updateUserRoute.ts
+++ src/http/users/updateUserRoute.ts
@@ -9,29 +9,27 @@ export async function updateUserRoute(fastify:FastifyInstance) {
     fastify.put('/usuarios/id/:id', async (request, reply) => {
         try {
             const userId = (request.params as { id: string }).id
-            const {
-                status: getStatus, 
-                data: user, 
-                message: getMessage, 
-                error: getError
-            } = await getUserById(userId)
-            if (!user || getError) {
-                return reply.status(getStatus).send({ message: getMessage })
-            }
 
-            const userDataValidate = await schemaCadastre.validateAsync(request.body)
-            const { firstName, lastName, email, phoneNumber } = userDataValidate
+            // 1. Verifica se o usuário existe
+            const userResponse = await getUserById(userId)
+            if (!userResponse.data || userResponse.error) {
+                return reply.status(userResponse.status).send({ message: userResponse.message })
+            }
+            const user = userResponse.data
+
+            // 2. Valida o corpo da requisição com schemaCadastre
+            const userData = await schemaCadastre.validateAsync(request.body)
+            const { firstName, lastName, email, phoneNumber } = userData
             
-            const { 
-                status: existingUserStatus, 
-                existingUser, 
-                message: existingUserMessage, 
-                error: existingUserError
-            } = await checkExistingUser(email)
-            if (existingUser || existingUserError) {
-                return reply.status(existingUserStatus).send({ message: existingUserMessage })
+            // 3. Checa se o email já existe, exceto para o email atual do usuário
+            if (email !== user.email){
+                const emailCheckResponse = await checkExistingUser(email)
+                if (emailCheckResponse.existingUser || emailCheckResponse.error) {
+                    return reply.status(emailCheckResponse.status).send({ message: emailCheckResponse.message })
+                }
             }
 
+            // 4. Atualiza o usuário no banco de dados
             await prisma.user.update({
                 where: {
                     userId: user.userId
@@ -42,10 +40,12 @@ export async function updateUserRoute(fastify:FastifyInstance) {
                     email,
                     phoneNumber
                 }
-            }).then(() => {
-                return reply.status(200).send({ message: 'Usuário atualizado com sucesso' })
             })
+
+            // 5. Responde com sucesso
+            return reply.status(200).send({ message: 'Usuário atualizado com sucesso' })
         } catch (error) {
+            // 6. Tratamento de erros genéricos
             return handleError(error, reply)
         }
     })
