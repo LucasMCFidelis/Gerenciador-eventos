4adfec426b3c974fdfce0a818c7944c861c42b4a
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-04 19:59:06 -0300

    Cria schema de validaçã para o id do usuario, e implementa validação na rota getUserRoute

--- src/http/users/getUserRoute.ts
+++ src/http/users/getUserRoute.ts
@@ -1,26 +1,31 @@
 import { FastifyInstance } from "fastify"
 import { getUserById } from "../../utils/db/getUserById.js"
 import { handleError } from "../../utils/handlers/handleError.js"
+import { schemaUserId } from "../../schemas/schemaUserId.js"
 
 export async function getUserRoute(fastify: FastifyInstance) {
-    fastify.get('/usuarios/id/:id', async (request, reply) => {
+    fastify.get<{ Params: { id: string } }>('/usuarios/id/:id', async (request, reply) => {
         try {
-            // 1. Verifica se o parâmetro id foi fornecido
-            const userId = (request.params as { id: string }).id
-            if (!userId) {
-                return reply.status(400).send({ message: 'ID deve ser fornecido' })
+            // 1. Extrair o parâmetro ID
+            const userId = request.params.id;
+
+            // 2. Validar o ID utilizando o schemaUserId
+            const { error } = schemaUserId.validate({ id: userId });
+            if (error) {
+                // Se o ID for inválido, retornar a mensagem de erro personalizada
+                return reply.status(400).send({ message: error.message });
             }
 
-            // 2. Verifica se o usuário existe
+            // 3. Verifica se o usuário existe
             const userResponse = await getUserById(userId)
             if (!userResponse.data || userResponse.error) {
                 return reply.status(userResponse.status).send({ message: userResponse.message })
             }
 
-            // 3. Responde com sucesso, devolvendo o usuário encontrado
+            // 4. Responde com sucesso, devolvendo o usuário encontrado
             return reply.status(200).send(userResponse.data)
         } catch (error: unknown) {
-            // 4. Tratamento de erros genéricos
+            // 5. Tratamento de erros genéricos
             return handleError(error, reply)
         }
     })
