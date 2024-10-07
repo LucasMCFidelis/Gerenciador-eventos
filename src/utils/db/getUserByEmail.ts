17d2c9e10897909b6e5f4d8c2fe0067eddf33bb9
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-06 21:45:34 -0300

    Ajusta a estruturação de retorno da função getUserByEmail, tornando-a mais detalhada

--- src/utils/db/getUserByEmail.ts
+++ src/utils/db/getUserByEmail.ts
@@ -6,13 +6,27 @@ interface UserAutentication {
     password: string
 }
 
-export async function getUserByEmail(userEmail: string): Promise<UserAutentication | null> {
-    if (!userEmail) {
-        console.error('Invalid userEmail')
+interface GetUserResponse {
+    status: number
+    data?: UserAutentication | null
+    message?: string
+    error?: boolean
+}
+
+export async function getUserByEmail(userEmail: string): Promise<GetUserResponse> {
+    // Validação inicial de entrada
+    if (!userEmail || typeof userEmail !== "string") {
+        return {
+            status: 400,
+            message: "Email inválido",
+            error: true,
+            data: null
+        };
     }
 
     try {
-        return await prisma.user.findUnique({
+        // Buscar usuário no banco de dados com base no email fornecido
+        const user = await prisma.user.findUnique({
             where: {
                 email: userEmail
             },
@@ -22,8 +36,31 @@ export async function getUserByEmail(userEmail: string): Promise<UserAutenticati
                 password: true
             }
         })
+
+        // Retorno caso o usuário não seja encontrado
+        if (!user){
+            return {
+                status: 404,
+                message: "Nenhum usuário encontrado com este email",
+                error: true,
+                data: null
+            }
+        }
+
+        // Retorno bem-sucedido com os dados do usuário
+        return {
+            status: 200,
+            data: user,
+            message: "Usuário encontrado com sucesso",
+            error: false
+        }
     } catch (error) {
         console.error("Erro ao buscar usuário", error)
-        return null
+        return {
+            status: 500,
+            message: "Erro interno ao buscar usuário",
+            error: true,
+            data: null
+        }
     }
 }
