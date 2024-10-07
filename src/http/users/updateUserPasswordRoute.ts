8a286311a0694b8fbf212fa5d940e17782f38219
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-07 18:24:10 -0300

    Ajustes e comentarios em updateUserPasswordRoute

--- src/http/users/updateUserPasswordRoute.ts
+++ src/http/users/updateUserPasswordRoute.ts
@@ -4,26 +4,33 @@ import { handleError } from "../../utils/handlers/handleError.js";
 import { getUserByEmail } from "../../utils/db/getUserByEmail.js";
 import { updateUserPassword } from "../../utils/db/updateUserPassword.js";
 
-export async function updateUserPasswordRoute(fastify:FastifyInstance) {
+export async function updateUserPasswordRoute(fastify: FastifyInstance) {
     // ADICIONAR VALIDAÇÃO POR CÓDIGO EM EMAIL OU ALGUMA OUTRA OPÇÃO
     fastify.patch('/usuarios/recuperacao/esqueci-senha', async (request, reply) => {
         try {
-            const { email, novaSenha } = request.body as { email: string, novaSenha: string }
+            // Extrair email e senha fornecida do corpo da requisição
+            const { email, newPassword } = request.body as { email: string, newPassword: string }
+
+            // Buscar o usuário no banco de dados utilizando a função utilitária
             const userResponse = await getUserByEmail(email)
             const user = userResponse.data
-
             if (!user || userResponse.error) {
                 return reply.status(userResponse.status).send({ message: userResponse.message })
             }
 
-            await schemaUserPassword.validateAsync({ password: novaSenha })
-            const updateResponse =await updateUserPassword(user.userId, novaSenha)
+            // Validar a nova senha com o schemaUserPassword para que a senha seja segura
+            await schemaUserPassword.validateAsync({ password: newPassword })
+
+            // Atualizar a senha do usuário no banco de dados utilizando a função utilitária
+            const updateResponse = await updateUserPassword(user.userId, newPassword)
             if (updateResponse.error) {
-                return reply.status(updateResponse.status).send({message: updateResponse.message})
+                return reply.status(updateResponse.status).send({ message: updateResponse.message })
             }
 
-            return reply.status(200).send({message: 'Senha atualizada com sucesso'})
+             // Retornar sucesso ap atualizar senha
+            return reply.status(200).send({ message: 'Senha atualizada com sucesso' })
         } catch (error) {
+            // Tratamento de erros genéricos utilizando o handler global
             return handleError(error, reply)
         }
     })
