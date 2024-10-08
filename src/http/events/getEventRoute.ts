39b99fd1dd30f6bd74826e059c35735535cca00e
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-08 17:23:54 -0300

    Ajusta rota debusca de usuário para tratar o retorno da função getEventById

--- src/http/events/getEventRoute.ts
+++ src/http/events/getEventRoute.ts
@@ -4,6 +4,12 @@ import { getEventById } from "../../utils/db/getEventById.js"
 export async function getEventRoute(fastify: FastifyInstance) {
     fastify.get('/eventos/:id', async (request, reply) => {
         const eventId = (request.params as { id: string }).id
-        await getEventById(eventId, reply)
+
+        const eventResponse = await getEventById(eventId)
+        if (!eventResponse.data || eventResponse.error){
+            return reply.status(eventResponse.status).send({message: eventResponse.message})
+        }
+
+        return reply.status(200).send(eventResponse.data)
     })
 }
