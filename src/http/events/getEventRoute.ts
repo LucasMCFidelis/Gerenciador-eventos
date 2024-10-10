ff6e8c4641930aaa30cbcb1b2546768a0cb2737a
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-10 19:23:29 -0300

    Ajusta rota getEventById e adiciona captação e tratamento de erros com handleError global

--- src/http/events/getEventRoute.ts
+++ src/http/events/getEventRoute.ts
@@ -1,15 +1,24 @@
 import { FastifyInstance } from "fastify"
 import { getEventById } from "../../utils/db/getEventById.js"
+import { handleError } from "../../utils/handlers/handleError.js"
 
 export async function getEventRoute(fastify: FastifyInstance) {
-    fastify.get('/eventos/:id', async (request, reply) => {
-        const eventId = (request.params as { id: string }).id
+    fastify.get<{ Params: { id: string } }>('/eventos/:id', async (request, reply) => {
+        try {
+            // Extrai o Id do evento dos parâmetros da rota
+            const eventId = request.params.id
 
-        const eventResponse = await getEventById(eventId)
-        if (!eventResponse.data || eventResponse.error){
-            return reply.status(eventResponse.status).send({message: eventResponse.message})
+            // Buscar o evento no banco de dados utilizando a função utilitária
+            const eventResponse = await getEventById(eventId)
+            if (!eventResponse.data || eventResponse.error) {
+                return reply.status(eventResponse.status).send({ message: eventResponse.message })
+            }
+
+            // Responder com os dados do usuário encontrado
+            return reply.status(200).send(eventResponse.data)
+        } catch (error) {
+            // Tratamento de erros genéricos utilizando o handler global
+            return handleError(error, reply)
         }
-
-        return reply.status(200).send(eventResponse.data)
     })
 }
