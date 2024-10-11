00f53311439103c4a273428d9cb9839f76b0cd79
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-10 22:20:38 -0300

    Refatora e comenta etapas de deleteEventRoute

--- src/http/events/deleteEventRoute.ts
+++ src/http/events/deleteEventRoute.ts
@@ -5,11 +5,16 @@ import { verifyRole } from "../../utils/security/verifyRole.js"
 import { checkExistingEvent } from "../../utils/validators/checkExistingEvent.js"
 
 export async function deleteEventRoute(fastify: FastifyInstance) {
-    fastify.delete('/eventos/id/:id', async (request, reply) => {
-        const eventId = (request.params as { id: string }).id
+    fastify.delete<{ 
+        Params: { id: string },
+        Body: { userId: string} 
+    }>('/eventos/:id', async (request, reply) => {
         try {
-            const { userId } = request.body as { userId: string }
+            // Extrai o Id do evento dos parâmetros da rota e o Id do usuário do corpo da requisição
+            const eventId = request.params.id
+            const userId = request.body.userId
 
+            // Valida a permissão do usuário
             const { status: roleStatus, hasPermission, message: roleMessage } = await verifyRole({
                 userId,
                 requiredRole: 'Admin'
@@ -18,22 +23,23 @@ export async function deleteEventRoute(fastify: FastifyInstance) {
                 return reply.status(roleStatus).send({ roleMessage })
             }
 
+            // Checa se o evento existe antes de tentar deletar do banco de dados
             const { status: eventStatus, eventExisting, message: eventMessage } = await checkExistingEvent(eventId)
             if (!eventExisting) {
                 return reply.status(eventStatus).send({ eventMessage })
             }
 
+            // Deleta o Evento
             await prisma.event.delete({
                 where: {
                     eventId
                 }
-            }).then(() => {
-                return reply.status(204).send()
-            }).catch((error) => {
-                console.error(error.message)
-                return reply.status(500).send({ message: 'Erro ao excluir evento' })
             })
+
+            // Retorna sucesso ao deletar evento
+            return reply.status(204).send()
         } catch (error) {
+            // Tratamento de erros genéricos utilizando o handler global
             return handleError(error, reply)
         }
     })
