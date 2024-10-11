ab4d3ac24ce7e64753aad40586c062295780db22
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-11 10:01:09 -0300

    Ajusta rota updateEventRoute para receber uma versão parcial da interface Event, validar os dados com o schemaEventUpdate e atualizar apenas os campos fornecidos

--- src/http/events/updateEventRoute.ts
+++ src/http/events/updateEventRoute.ts
@@ -1,61 +1,75 @@
 import { FastifyInstance } from "fastify"
-import { schemaEvent } from "../../schemas/schemaEvent.js"
 import { prisma } from "../../utils/db/prisma.js"
 import { handleError } from "../../utils/handlers/handleError.js"
 import { verifyRole } from "../../utils/security/verifyRole.js"
 import { checkExistingEvent } from "../../utils/validators/checkExistingEvent.js"
-import { Event } from "../../interfaces/eventInterface.js"
+import { EventRequestBody } from "../../interfaces/eventRequestBodyInterface.js"
+import { schemaEventUpdate } from "../../schemas/schemaEventUpdate.js"
 
 export async function UpdateEventRoute(fastify: FastifyInstance) {
-    fastify.put('/eventos/id/:id', async (request, reply) => {
+    fastify.put<{
+        Params: { id: string },
+        Body: Partial<EventRequestBody>
+    }>('/eventos/:id', async (request, reply) => {
         try {
-            const { userId, title, description, linkEvent, address, startDateTime, endDateTime } = request.body as Event
-            const eventId = (request.params as { id: string }).id
+            // Extrai o Id do evento dos parâmetros da rota e os dados do corpo da requisição de acordo com EventRequestBody
+            const { userId, title, description, linkEvent, address, startDateTime, endDateTime, accessibilityLevel } = request.body
+            const eventId = request.params.id
 
+            // Verifica se userId está presente
+            if (!userId) {
+                return reply.status(400).send({ message: "userId é obrigatório" })
+            }
+
+            // Valida a permissão do usuário
             const { status: roleStatus, hasPermission, message: roleMessage } = await verifyRole({
                 userId,
                 requiredRole: 'Admin'
             })
             if (!hasPermission) {
-                return reply.status(roleStatus).send({ roleMessage })
+                return reply.status(roleStatus).send({ message: roleMessage })
             }
 
-            await schemaEvent.validateAsync({
+            // Validar os dados fornecidos no corpo da requisição
+            await schemaEventUpdate.validateAsync({
                 title,
                 description,
                 linkEvent,
                 address,
                 startDateTime,
-                endDateTime
+                endDateTime,
+                accessibilityLevel
             })
 
+            // Checa se o evento com id fornecido existe
             const { status: eventStatus, eventExisting, message: eventMessage } = await checkExistingEvent(eventId)
             if (!eventExisting) {
                 return reply.status(eventStatus).send({ eventMessage })
             }
 
-            await prisma.event.update({
+            // Atualiza evento no banco de dados
+            const updatedEvent = await prisma.event.update({
                 data: {
-                    title,
-                    description,
-                    linkEvent,
-                    street: address.street,
-                    number: address.number,
-                    neighborhood: address.neighborhood,
-                    complement: address.complement,
-                    startDateTime,
-                    endDateTime
+                    ...(title && { title }),
+                    ...(description && { description }),
+                    ...(linkEvent && { linkEvent }),
+                    ...(address?.street && { street: address.street }),
+                    ...(address?.number && { number: address.number }),
+                    ...(address?.neighborhood && { neighborhood: address.neighborhood }),
+                    ...(address?.complement && { complement: address.complement }),
+                    ...(startDateTime && { startDateTime }),
+                    ...(endDateTime && { endDateTime }),
+                    ...(accessibilityLevel && { accessibilityLevel })
                 },
                 where: {
                     eventId
                 }
-            }).then(() => {
-                return reply.status(204).send()
-            }).catch((error) => {
-                console.error(error.message)
-                return reply.status(500).send({ message: 'Erro ao atualizar evento' })
             })
+
+            // Retorna sucesso ao atualizar evento
+            return reply.status(200).send(updatedEvent)
         } catch (error) {
+            // Tratamento de erros genéricos utilizando o handler global
             return handleError(error, reply)
         }
     })
