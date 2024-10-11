804ac29b7176b6c2636d7bdc697d56661a049c75
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-11 19:48:01 -0300

    Cria e implementa schema separado para validação dos campos da requisição do usuário na rota de atualização de usuários

--- src/http/users/updateUserRoute.ts
+++ src/http/users/updateUserRoute.ts
@@ -1,14 +1,18 @@
 import { FastifyInstance } from "fastify";
 import { checkExistingUser } from "../../utils/validators/checkExistingUser.js";
-import { schemaCadastre } from "../../schemas/schemaCadastre.js";
 import { getUserById } from "../../utils/db/getUserById.js";
 import { handleError } from "../../utils/handlers/handleError.js";
 import { prisma } from "../../utils/db/prisma.js";
+import { schemaUserUpdate } from "../../schemas/schemaUserUpdate.js";
+import { CadastreUser } from "../../interfaces/cadastreUserInterface.js";
 
 export async function updateUserRoute(fastify:FastifyInstance) {
-    fastify.put('/usuarios/id/:id', async (request, reply) => {
+    fastify.put<{
+        Params: {id: string},
+        Body: Partial<CadastreUser>
+    }>('/usuarios/:id', async (request, reply) => {
         try {
-            const userId = (request.params as { id: string }).id
+            const userId = request.params.id
 
             // 1. Verifica se o usuário existe
             const userResponse = await getUserById(userId)
@@ -18,7 +22,7 @@ export async function updateUserRoute(fastify:FastifyInstance) {
             const user = userResponse.data
 
             // 2. Valida o corpo da requisição com schemaCadastre
-            const userData = await schemaCadastre.validateAsync(request.body)
+            const userData = await schemaUserUpdate.validateAsync(request.body)
             const { firstName, lastName, email, phoneNumber } = userData
             
             // 3. Checa se o email já existe, exceto para o email atual do usuário
