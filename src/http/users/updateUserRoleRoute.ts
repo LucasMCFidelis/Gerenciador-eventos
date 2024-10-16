23a4213b84dfaa8af11d26fec8a679b32fb3b24e
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-16 20:13:32 -0300

    Ajusta Rota de atualização de permissão do usuário para usar a função getRoleByName

--- src/http/users/updateUserRoleRoute.ts
+++ src/http/users/updateUserRoleRoute.ts
@@ -3,35 +3,32 @@ import { verifyRole } from "../../utils/security/verifyRole.js";
 import { getUserById } from "../../utils/db/getUserById.js";
 import { prisma } from "../../utils/db/prisma.js";
 import { handleError } from "../../utils/handlers/handleError.js";
-
-enum UserRole {
-    Admin = "Admin",
-    User = "User"
-}
+import { getRoleByName, UserRole } from "../../utils/db/getRoleByName.js";
 
 export async function updateUserRoleRoute(fastify: FastifyInstance) {
     fastify.put<{
         Params: { id: string },
         Body: {
             adminId: string,
-            newRole: string
+            newRole: UserRole
         }
     }>('/usuarios/:id/permissao', async (request, reply) => {
         try {
             const userId = request.params.id
-            const {adminId, newRole} = request.body
-    
-            // Validação do newRole usando o enum
-            if (!(newRole in UserRole)) {
-                return reply.status(400).send({ message: "Papel inválido. Somente 'Admin' ou 'User' são aceitos." })
+            const { adminId, newRole } = request.body
+
+            // Validação do newRole
+            const roleResponse = await getRoleByName(newRole)
+            if (!roleResponse.data || roleResponse.error) {
+                return reply.status(roleResponse.status).send({ message: roleResponse.message })
             }
 
             // Verificar permissão do usuário
-            const verifyRoleResponse = await verifyRole({userId: adminId, requiredRole: "Admin"})
+            const verifyRoleResponse = await verifyRole({ userId: adminId, requiredRole: "Admin" })
             if (!verifyRoleResponse.hasPermission || verifyRoleResponse.error) {
-                return reply.status(verifyRoleResponse.status).send({ message : verifyRoleResponse.message })
-            } 
-     
+                return reply.status(verifyRoleResponse.status).send({ message: verifyRoleResponse.message })
+            }
+
             // Verifica se o usuário existe
             const userResponse = await getUserById(userId)
             const user = userResponse.data
@@ -39,20 +36,19 @@ export async function updateUserRoleRoute(fastify: FastifyInstance) {
                 return reply.status(userResponse.status).send({ message: userResponse.message })
             }
 
-            if (user.role?.roleName === newRole){
-                return reply.status(400).send({ message: `O usuário ${user.firstName} já possui a permissão ${newRole}`})
+            if (user.role?.roleName === newRole) {
+                return reply.status(409).send({ message: `O usuário ${user.firstName} já possui a permissão ${newRole}` })
             }
-    
+
             const updatedUser = await prisma.user.update({
-                where: {userId},
-                data: {roleId: (newRole === UserRole.Admin ? 1 : 2)}
+                where: { userId },
+                data: { roleId: roleResponse.data.roleId }
             })
-    
+
             return reply.status(200).send({
                 message: `Permissão do usuário ${updatedUser.firstName} atualizada com sucesso para ${newRole}`
             })
-        } catch (error){
-            console.error(error)
+        } catch (error) {
             handleError(error, reply)
         }
     })
