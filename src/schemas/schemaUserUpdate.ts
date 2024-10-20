e93939abc7e6526dfcb70a567de6877f0ca8da9b
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-20 20:49:26 -0300

    Adiciona mais mensagens na validação do schemaUserUpdate

--- src/schemas/schemaUserUpdate.ts
+++ src/schemas/schemaUserUpdate.ts
@@ -6,12 +6,16 @@ export const schemaUserUpdate = Joi.object({
         (value) => removeWhitespace(value)
     ).min(3).pattern(new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$')).optional().messages({
         'string.base': 'Nome deve ser uma string',
+        'string.empty': 'Nome não pode estar vazio',
+        'string.min': 'Nome deve possuir no mínimo 3 caracteres',
         'string.pattern.base': 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços'
     }),
     lastName: Joi.string().custom(
         (value) => removeWhitespace(value)
     ).min(5).pattern(new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$')).optional().messages({
         'string.base': 'Sobrenome deve ser uma string',
+        'string.empty': 'Sobrenome não pode estar vazio',
+        'string.min': 'Sobrenome deve possuir no mínimo 5 caracteres',
         'string.pattern.base': 'Sobrenome deve conter apenas caracteres alfabéticos, acentuados e espaços'
     }),
     email: Joi.string().email().optional().messages({
