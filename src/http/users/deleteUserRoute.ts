a50b32491e9e9832527778ea65465c9b7f1ef62e
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-23 22:22:38 -0300

    documenta rota deleteUserRoute

--- src/http/users/deleteUserRoute.ts
+++ src/http/users/deleteUserRoute.ts
@@ -3,8 +3,48 @@ import { getUserById } from "../../utils/db/getUserById.js"
 import { handleError } from "../../utils/handlers/handleError.js"
 import { prisma } from "../../utils/db/prisma.js"
 
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   delete:
+ *     summary: Deletar um usuário
+ *     description: Deleta um usuário específico pelo ID.
+ *     tags:
+ *       - Usuários
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do usuário a ser deletado
+ *     responses:
+ *       204:
+ *         description: Usuário deletado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Usuário excluído com sucesso
+ *       400:
+ *         description: Erro de validação
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *       404:
+ *         description: Usuário não encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
+
 export async function deleteUserRoute(fastify: FastifyInstance) {
-    fastify.delete<{Params: {id: string}}>('/usuarios/:id', async (request, reply) => {
+    fastify.delete<{ Params: { id: string } }>('/usuarios/:id', async (request, reply) => {
         try {
             // Extrair o ID do usuário a partir dos parâmetros da rota
             const userId = request.params.id
@@ -23,7 +63,7 @@ export async function deleteUserRoute(fastify: FastifyInstance) {
             })
 
             // Retornar sucesso ao deletar usuário
-            return reply.status(204).send({message: 'Usuário excluído com sucesso'})
+            return reply.status(204).send({ message: 'Usuário excluído com sucesso' })
         } catch (error) {
             // Tratamento de erros genéricos utilizando o handler global
             return handleError(error, reply)
