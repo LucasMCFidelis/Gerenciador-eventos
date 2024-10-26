420ff54a6ba2edd9dd15d6d5238033bbc45c7651
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-25 22:43:26 -0300

    Ajusta formatação/identação do arquivo swagger.ts onde são armazenadps os schemas para documentação

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -2,46 +2,45 @@ export const swaggerSchemaDeleteUser = {
   description: 'Deletar um usuário específico pelo ID.',
   tags: ['Usuários'],
   params: {
-      type: 'object',
-      required: ['id'],
-      properties: {
-          id: {
-              type: 'string',
-              description: 'ID do usuário a ser deletado.',
-          },
+    type: 'object',
+    required: ['id'],
+    properties: {
+      id: {
+        description: 'ID do usuário a ser deletado.',
       },
+    },
   },
   response: {
-      204: {
-          description: 'Usuário deletado com sucesso.',
+    204: {
+      description: 'Usuário deletado com sucesso.',
+    },
+    400: {
+      description: 'Requisição inválida.',
+      type: 'object',
+      properties: {
+        message: { type: 'string' }
       },
-      400: {
-          description: 'Requisição inválida.',
-          type: 'object',
-          properties: {
-              message: { type: 'string' }
-          },
-          examples: [
-              {
-                  message: "O ID não pode ser vazio."
-              },
-              {
-                  message: "O ID deve estar no formato de UUID v4."
-              }
-          ]
+      examples: [
+        {
+          message: "O ID não pode ser vazio."
+        },
+        {
+          message: "O ID deve estar no formato de UUID v4."
+        }
+      ]
+    },
+    404: {
+      description: 'Usuário não encontrado.',
+      type: 'object',
+      properties: {
+        message: { type: 'string' }
       },
-      404: {
-          description: 'Usuário não encontrado.',
-          type: 'object',
-          properties: {
-              message: { type: 'string' }
-          },
-          examples: [
-              {
-                  message: "Usuário não encontrado."
-              }
-          ]
-      }
+      examples: [
+        {
+          message: "Usuário não encontrado."
+        }
+      ]
+    }
   }
 }
 
@@ -49,71 +48,71 @@ export const swaggerSchemaCreateUser = {
   description: 'Cadastro de um novo usuário.',
   tags: ['Usuários'],
   body: {
-      type: 'object',
-      required: ['firstName', 'lastName', 'email', 'password'],
-      properties: {
-          firstName: {
-              description: 'Primeiro nome do usuário. Deve conter apenas letras e espaços.',
-          },
-          lastName: {
-              description: 'Sobrenome do usuário. Deve conter apenas letras e espaços.',
-          },
-          email: {
-              description: 'Endereço de email válido.',
-          },
-          phoneNumber: {
-              description: 'Número de telefone. Deve começar com "+" e conter entre 10 e 15 dígitos.',
-          },
-          password: {
-              description: 'Senha do usuário. Deve conter no mínimo 6 caracteres.',
-          }
+    type: 'object',
+    required: ['firstName', 'lastName', 'email', 'password'],
+    properties: {
+      firstName: {
+        description: 'Primeiro nome do usuário. Deve conter apenas letras e espaços.',
       },
-      examples: [
-          {
-              firstName: 'Lucas',
-              lastName: 'Fidelis',
-              email: 'lucas.fidelis@example.com',
-              phoneNumber: '+5511999998888',
-              password: 'senhaSecreta123$'
-          }
-      ]
+      lastName: {
+        description: 'Sobrenome do usuário. Deve conter apenas letras e espaços.',
+      },
+      email: {
+        description: 'Endereço de email válido.',
+      },
+      phoneNumber: {
+        description: 'Número de telefone. Deve começar com "+" e conter entre 10 e 15 dígitos.',
+      },
+      password: {
+        description: 'Senha do usuário. Deve conter no mínimo 6 caracteres.',
+      }
+    },
+    examples: [
+      {
+        firstName: 'Lucas',
+        lastName: 'Fidelis',
+        email: 'lucas.fidelis@example.com',
+        phoneNumber: '+5511999998888',
+        password: 'senhaSecreta123$'
+      }
+    ]
   },
   response: {
-      201: {
-          description: 'Usuário cadastrado com sucesso.',
-          type: 'object',
-          properties: {
-              userId: { type: 'string' },
-              firstName: { type: 'string' },
-              lastName: { type: 'string' },
-              email: { type: 'string' },
-              phoneNumber: { type: 'string' }
-          },
-          examples: [
-              {
-                  userId: '12345',
-                  firstName: 'Lucas',
-                  lastName: 'Fidelis',
-                  email: 'lucas.fidelis@example.com'
-              }
-          ]
+    201: {
+      description: 'Usuário cadastrado com sucesso.',
+      type: 'object',
+      properties: {
+        userId: { type: 'string' },
+        firstName: { type: 'string' },
+        lastName: { type: 'string' },
+        email: { type: 'string' },
+        phoneNumber: { type: 'string' }
       },
-      400: {
-          description: 'Erro de validação.',
-          type: 'object',
-          properties: {
-              message: { type: 'string' }
-          },
-          examples: [
-              {
-                  message: 'Nome deve ser uma string'
-              },
-              {
-                  message: 'Nome deve possuir no mínimo 3 caracteres'
-              },
+      examples: [
+        {
+          userId: '12345',
+          firstName: 'Lucas',
+          lastName: 'Fidelis',
+          email: 'lucas.fidelis@example.com'
+        }
+      ]
+    },
+    400: {
+      description: 'Erro de validação.',
+      type: 'object',
+      properties: {
+        message: { type: 'string' }
+      },
+      examples: [
+        {
+          message: 'Nome deve ser uma string'
+        },
+        {
+          message: 'Nome deve possuir no mínimo 3 caracteres'
+        },
 
-          ]
-      },
+      ]
+    },
   }
 }
 
@@ -121,53 +120,52 @@ export const swaggerSchemaGetUser = {
   description: 'Retorna um usuário específico pelo ID.',
   tags: ['Usuários'],
   params: {
-      type: 'object',
-      required: ['id'],
-      properties: {
-          id: {
-              type: 'string',
-              description: 'ID do usuário a ser obtido.',
-          },
+    type: 'object',
+    required: ['id'],
+    properties: {
+      id: {
+        description: 'ID do usuário a ser obtido.',
       },
+    },
   },
   response: {
-      200: {
-          description: 'Usuário encontrado.',
-          type: 'object',
-          properties: {
-              firstName: {
-                  description: 'Primeiro nome do usuário.'
-              },
-              lastName: {
-                  description: 'Sobrenome do usuário.'
-              },
-              email: {
-                  description: 'Email do usuário.'
-              },
-              phoneNumber: {
-                  description: 'Telefone do usuário, opcional para contato.'
-              }
-          },
-          examples: [
-              {
-                  firstName: 'Lucas',
-                  lastName: 'Fidelis',
-                  email: 'lucas.fidelis@example.com',
-                  phoneNumber: "+5511999998888"
-              }
-          ]
+    200: {
+      description: 'Usuário encontrado.',
+      type: 'object',
+      properties: {
+        firstName: {
+          description: 'Primeiro nome do usuário.'
+        },
+        lastName: {
+          description: 'Sobrenome do usuário.'
+        },
+        email: {
+          description: 'Email do usuário.'
+        },
+        phoneNumber: {
+          description: 'Telefone do usuário, opcional para contato.'
+        }
       },
-      404: {
-          description: 'Usuário não encontrado.',
-          type: 'object',
-          properties: {
-              message: { type: 'string' }
-          },
-          examples: [
-              {
-                  message: "Usuário não encontrado."
-              }
-          ]
-      }
+      examples: [
+        {
+          firstName: 'Lucas',
+          lastName: 'Fidelis',
+          email: 'lucas.fidelis@example.com',
+          phoneNumber: "+5511999998888"
+        }
+      ]
+    },
+    404: {
+      description: 'Usuário não encontrado.',
+      type: 'object',
+      properties: {
+        message: { type: 'string' }
+      },
+      examples: [
+        {
+          message: "Usuário não encontrado."
+        }
+      ]
+    }
   }
 }
