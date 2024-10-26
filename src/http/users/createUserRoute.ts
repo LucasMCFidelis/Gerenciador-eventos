29beb243772da37daa5354d44d3143f07a508a44
Lucas Fidelis <lucasm241301@gmail.com>
2024-10-25 22:24:25 -0300

    Documenta rota de cadastro de usuário

--- src/http/users/createUserRoute.ts
+++ src/http/users/createUserRoute.ts
@@ -6,98 +6,11 @@ import { handleError } from "../../utils/handlers/handleError.js";
 import { prisma } from "../../utils/db/prisma.js";
 import { hashPassword } from "../../utils/security/hashPassword.js";
 import { CadastreUser } from "../../interfaces/cadastreUserInterface.js";
+import { swaggerSchemaCreateUser } from "../../utils/swagger.js";
 
 export async function createUserRoute(fastify: FastifyInstance) {
     fastify.post('/usuarios', {
-        schema: {
-            description: 'Cadastro de um novo usuário.',
-            tags: ['Usuários'],
-            body: {
-                type: 'object',
-                required: ['firstName', 'lastName', 'email', 'password'], // Adicionando 'password'
-                properties: {
-                    firstName: {
-                        type: 'string',
-                        minLength: 3,
-                        pattern: '^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$',
-                        description: 'Primeiro nome do usuário. Deve conter apenas letras e espaços.',
-                    },
-                    lastName: {
-                        type: 'string',
-                        minLength: 5,
-                        pattern: '^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$',
-                        description: 'Sobrenome do usuário. Deve conter apenas letras e espaços.',
-                    },
-                    email: {
-                        type: 'string',
-                        format: 'email',
-                        description: 'Endereço de email válido.',
-                    },
-                    phoneNumber: {
-                        type: 'string',
-                        pattern: '^\\+?[0-9]{10,15}$',
-                        nullable: true,
-                        description: 'Número de telefone. Deve começar com "+" e conter entre 10 e 15 dígitos.',
-                    },
-                    password: {
-                        type: 'string',
-                        minLength: 8,
-                        description: 'Senha do usuário. Deve conter no mínimo 6 caracteres.',
-                    }
-                },
-                examples: [
-                    {
-                        summary: 'Exemplo válido',
-                        value: {
-                            firstName: 'Lucas',
-                            lastName: 'Fidelis',
-                            email: 'lucas.fidelis@example.com',
-                            phoneNumber: '+5511999998888',
-                            password: 'senhaSecreta123$'
-                        }
-                    }
-                ]
-            },
-            response: {
-                201: {
-                    description: 'Usuário cadastrado com sucesso.',
-                    type: 'object',
-                    properties: {
-                        userId: { type: 'string' },
-                        firstName: { type: 'string' },
-                        lastName: { type: 'string' },
-                        email: { type: 'string' },
-                        phoneNumber: { type: 'string' }
-                    },
-                    examples: [
-                        {
-                            summary: 'Resposta bem-sucedida',
-                            value: {
-                                userId: '12345',
-                                firstName: 'Lucas',
-                                lastName: 'Fidelis',
-                                email: 'lucas.fidelis@example.com'
-                            }
-                        }
-                    ]
-                },
-                400: {
-                    description: 'Erro de validação.',
-                    type: 'object',
-                    properties: {
-                        message: { type: 'string' }
-                    },
-                    examples: [
-                        {
-                            summary: 'Erro de validação',
-                            value: {
-                                message: 'Nome deve possuir no mínimo 3 caracteres'
-                            }
-                        }
-                    ]
-                }
-            }
-        }
+        schema: swaggerSchemaCreateUser
     }, async (request, reply) => {
         try {
             // Extrair dados fornecidos no corpo da requisição
--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -44,3 +44,75 @@ export const swaggerSchemaDeleteUser = {
       }
   }
 }
+
+export const swaggerSchemaCreateUser = {
+  description: 'Cadastro de um novo usuário.',
+  tags: ['Usuários'],
+  body: {
+      type: 'object',
+      required: ['firstName', 'lastName', 'email', 'password'],
+      properties: {
+          firstName: {
+              description: 'Primeiro nome do usuário. Deve conter apenas letras e espaços.',
+          },
+          lastName: {
+              description: 'Sobrenome do usuário. Deve conter apenas letras e espaços.',
+          },
+          email: {
+              description: 'Endereço de email válido.',
+          },
+          phoneNumber: {
+              description: 'Número de telefone. Deve começar com "+" e conter entre 10 e 15 dígitos.',
+          },
+          password: {
+              description: 'Senha do usuário. Deve conter no mínimo 6 caracteres.',
+          }
+      },
+      examples: [
+          {
+              firstName: 'Lucas',
+              lastName: 'Fidelis',
+              email: 'lucas.fidelis@example.com',
+              phoneNumber: '+5511999998888',
+              password: 'senhaSecreta123$'
+          }
+      ]
+  },
+  response: {
+      201: {
+          description: 'Usuário cadastrado com sucesso.',
+          type: 'object',
+          properties: {
+              userId: { type: 'string' },
+              firstName: { type: 'string' },
+              lastName: { type: 'string' },
+              email: { type: 'string' },
+              phoneNumber: { type: 'string' }
+          },
+          examples: [
+              {
+                  userId: '12345',
+                  firstName: 'Lucas',
+                  lastName: 'Fidelis',
+                  email: 'lucas.fidelis@example.com'
+              }
+          ]
+      },
+      400: {
+          description: 'Erro de validação.',
+          type: 'object',
+          properties: {
+              message: { type: 'string' }
+          },
+          examples: [
+              {
+                  message: 'Nome deve ser uma string'
+              },
+              {
+                  message: 'Nome deve possuir no mínimo 3 caracteres'
+              },
+
+          ]
+      },
+  }
+}
