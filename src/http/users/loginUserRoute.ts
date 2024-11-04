265fe26e41e33dd9595d0d56ec5a046894be428e
Lucas Fidelis <lucasm241301@gmail.com>
2024-11-04 20:14:39 -0300

    Ajusta rota de login para receber itens preload corretos para geração do token

--- src/http/users/loginUserRoute.ts
+++ src/http/users/loginUserRoute.ts
@@ -3,7 +3,7 @@ import { comparePasswords } from "../../utils/security/comparePasswords.js";
 import { getUserByEmail } from "../../utils/db/getUserByEmail.js";
 import { generateToken } from "../../utils/security/generateToken.js";
 
-export async function loginUserRoute(fastify:FastifyInstance) {
+export async function loginUserRoute(fastify: FastifyInstance) {
     // ADICIONAR LOGICA DE CRIAÇÃO DE TOKENS PARA PRÓXIMAS AUTENTICAÇÕES 
     fastify.post('/usuarios/login', async (request, reply) => {
         try {
@@ -11,24 +11,33 @@ export async function loginUserRoute(fastify:FastifyInstance) {
             const { email, senhaFornecida } = request.body as { email: string, senhaFornecida: string }
 
             // Buscar o usuário no banco de dados pelo email
-            const {status, data: user, message, error} = await getUserByEmail(email)
+            const userResponse = await getUserByEmail(email)            
 
             // Se houve um erro na busca ou usuário não existe
-            if (error || !user) {
+            if (userResponse.error || !userResponse.data) {
                 // Manter uma mensagem de erro padrão para casos onde `user` não é encontrado
-                const errorMessage = message || 'Credenciais inválidas'
-                return reply.status(error ? status : 401).send({ message: errorMessage })
+                const errorMessage = userResponse.message || 'Credenciais inválidas'
+                return reply.status(userResponse.error ? userResponse.status : 401).send({ message: errorMessage })
             }
 
             // Validar a senha fornecida com a armazenada no banco de dados
-            const passwordValid = await comparePasswords(senhaFornecida, user.password)
+            const passwordValid = await comparePasswords(senhaFornecida, userResponse.data.password)
 
             // Retornar erro de credenciais inválidas se a senha não for válida
             if (!passwordValid) {
                 return reply.status(401).send({ message: 'Credenciais inválidas' })
             }
 
-            const token = generateToken(fastify, user)
+            const user = {
+                userId: userResponse.data.userId,
+                email: userResponse.data.email,
+                roleName: userResponse.data.role.roleName
+            }
+
+            const token = generateToken(
+                fastify,
+                user
+            )
 
             // Retorno de sucesso com token de autenticação do usuário
             return reply.status(200).send({ message: 'Login bem-sucedido', userToken: token })
