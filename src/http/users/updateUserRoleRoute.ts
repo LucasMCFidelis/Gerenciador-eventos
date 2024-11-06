b6bfc864f4a952b67bd4c0e153bf61a9c3d01f98
Lucas Fidelis <lucasm241301@gmail.com>
2024-11-05 22:13:44 -0300

    Ajusta rota de atualização de permissão do usúario para usar a autenticação com checkRole

--- src/http/users/updateUserRoleRoute.ts
+++ src/http/users/updateUserRoleRoute.ts
@@ -1,35 +1,30 @@
 import { FastifyInstance } from "fastify";
-import { verifyRole } from "../../utils/security/verifyRole.js";
 import { getUserById } from "../../utils/db/getUserById.js";
 import { prisma } from "../../utils/db/prisma.js";
 import { handleError } from "../../utils/handlers/handleError.js";
 import { getRoleByName, UserRole } from "../../utils/db/getRoleByName.js";
+import { checkRole } from "../../utils/security/checkRole.js";
 
 export async function updateUserRoleRoute(fastify: FastifyInstance) {
     fastify.put<{
         Params: { id: string },
         Body: {
-            adminId: string,
             newRole: UserRole
         }
-    }>('/usuarios/:id/permissao', async (request, reply) => {
+    }>('/usuarios/:id/permissao', {
+        onRequest: [fastify.authenticate, await checkRole('Admin')]
+    }, async (request, reply) => {
         try {
              // Extrair o ID do usuário a partir dos parâmetros da rota
             const userId = request.params.id
 
-             // Extrair o ID do admin e nova permissão do usuário a partir do corpo da requisição
-            const { adminId, newRole } = request.body
+             // Extrair a nova permissão do usuário a partir do corpo da requisição
+            const { newRole } = request.body
 
             // Validação do newRole
             const roleResponse = await getRoleByName(newRole)
             if (!roleResponse.data || roleResponse.error) {
                 return reply.status(roleResponse.status).send({ message: roleResponse.message })
-            }
-
-            // Verificar permissão do usuário
-            const verifyRoleResponse = await verifyRole({ userId: adminId, requiredRole: "Admin" })
-            if (!verifyRoleResponse.hasPermission || verifyRoleResponse.error) {
-                return reply.status(verifyRoleResponse.status).send({ message: verifyRoleResponse.message })
             }
 
             // Verifica se o usuário existe
