656e844faf624dc684baa2ace414bc0eb40fafa9
Lucas Fidelis <lucasm241301@gmail.com>
2024-11-08 14:17:21 -0300

    Atualiza updateUserPasswordRoute para receber e validar o codigo de recuperação

--- src/http/users/updateUserPasswordRoute.ts
+++ src/http/users/updateUserPasswordRoute.ts
@@ -3,35 +3,58 @@ import { schemaUserPassword } from "../../schemas/schemaUserPassword.js";
 import { handleError } from "../../utils/handlers/handleError.js";
 import { getUserByEmail } from "../../utils/db/getUserByEmail.js";
 import { updateUserPassword } from "../../utils/db/updateUserPassword.js";
+import { validateRecoveryCode } from "../../utils/validators/validateRecoveryCode.js";
 
 export async function updateUserPasswordRoute(fastify: FastifyInstance) {
-    // ADICIONAR VALIDAÇÃO POR CÓDIGO EM EMAIL OU ALGUMA OUTRA OPÇÃO
-    fastify.patch('/usuarios/recuperacao/esqueci-senha', async (request, reply) => {
-        try {
-            // Extrair email e senha fornecida do corpo da requisição
-            const { email, newPassword } = request.body as { email: string, newPassword: string }
+  // ADICIONAR VALIDAÇÃO POR CÓDIGO EM EMAIL OU ALGUMA OUTRA OPÇÃO
+  fastify.patch<{
+    Body: {
+      email: string;
+      newPassword: string;
+      recoveryCode: string;
+    };
+  }>("/usuarios/recuperacao/atualizar-senha", async (request, reply) => {
+    try {
+      // Extrair email e senha fornecida do corpo da requisição
+      const { email, newPassword, recoveryCode } = request.body;
 
-            // Buscar o usuário no banco de dados utilizando a função utilitária
-            const userResponse = await getUserByEmail(email)
-            const user = userResponse.data
-            if (!user || userResponse.error) {
-                return reply.status(userResponse.status).send({ message: userResponse.message })
-            }
+      // Buscar o usuário no banco de dados utilizando a função utilitária
+      const userResponse = await getUserByEmail(email);
+      const user = userResponse.data;
+      if (!user || userResponse.error) {
+        return reply
+          .status(userResponse.status)
+          .send({ message: userResponse.message });
+      }
 
-            // Validar a nova senha com o schemaUserPassword para que a senha seja segura
-            await schemaUserPassword.validateAsync({ password: newPassword })
+      const validateCode = await validateRecoveryCode({
+        userEmail: email,
+        recoveryCode,
+      });
+      if (validateCode.error) {
+        return reply
+          .status(validateCode.status)
+          .send({ message: validateCode.message });
+      }
 
-            // Atualizar a senha do usuário no banco de dados utilizando a função utilitária
-            const updateResponse = await updateUserPassword(user.userId, newPassword)
-            if (updateResponse.error) {
-                return reply.status(updateResponse.status).send({ message: updateResponse.message })
-            }
+      // Validar a nova senha com o schemaUserPassword para que a senha seja segura
+      await schemaUserPassword.validateAsync({ password: newPassword });
 
-             // Retornar sucesso ap atualizar senha
-            return reply.status(200).send({ message: 'Senha atualizada com sucesso' })
-        } catch (error) {
-            // Tratamento de erros genéricos utilizando o handler global
-            return handleError(error, reply)
-        }
-    })
+      // Atualizar a senha do usuário no banco de dados utilizando a função utilitária
+      const updateResponse = await updateUserPassword(user.userId, newPassword);
+      if (updateResponse.error) {
+        return reply
+          .status(updateResponse.status)
+          .send({ message: updateResponse.message });
+      }
+
+      // Retornar sucesso ap atualizar senha
+      return reply
+        .status(200)
+        .send({ message: "Senha atualizada com sucesso" });
+    } catch (error) {
+      // Tratamento de erros genéricos utilizando o handler global
+      return handleError(error, reply);
+    }
+  });
 }
