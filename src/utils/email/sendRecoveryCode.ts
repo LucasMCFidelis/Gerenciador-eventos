2982af200b16f602e268c7f5bb94edfe72b4ae35
Lucas Fidelis <lucasm241301@gmail.com>
2024-11-09 15:16:10 -0300

    Adiciona e configura envio de email com o nodemailer

--- src/utils/email/sendRecoveryCode.ts
+++ src/utils/email/sendRecoveryCode.ts
@@ -41,17 +41,23 @@ export async function sendRecoveryCode(userEmail: string): Promise<GetResponse> 
     console.log(recoveryRecord);
 
     // Enviar o código de recuperação por e-mail (usando uma função de envio de e-mail)
-    // const emailSubject = "Código de Recuperação de Senha";
-    // const emailBody = `
-    //   <p>Olá,</p>
-    //   <p>Você solicitou a recuperação de senha. Use o código abaixo para prosseguir com a alteração de senha:</p>
-    //   <p><strong>${recoveryCode}</strong></p>
-    //   <p>Este código expirará em 1 hora.</p>
-    //   <p>Atenciosamente,</p>
-    //   <p>Equipe do Sistema</p>
-    // `;
+    const emailSubject = "Código de Recuperação de Senha";
+    const emailBody = `
+      <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+          <p style="font-size: 18px;">Olá,</p>
+          <p style="font-size: 16px;">Recebemos uma solicitação para a recuperação da sua senha. Para continuar, por favor, utilize o código abaixo:</p>
+          <div style="background-color: #f9f9f9; border: 1px solid #ddd; padding: 15px; border-radius: 5px; text-align: center; font-size: 20px; font-weight: bold; color: #2c3e50;">
+              ${recoveryCode}
+          </div>
+          <p style="font-size: 16px; margin-top: 10px;">⚠️ Lembre-se: Este código é válido por apenas <strong>1 hora</strong>. Após esse período, você precisará solicitar um novo código.</p>
+          <p style="font-size: 16px;">Se você não solicitou a recuperação de senha, desconsidere este e-mail.</p>
+          <p style="font-size: 16px;">Estamos aqui para ajudar! Se tiver alguma dúvida, não hesite em nos contatar.</p>
+          <p style="font-size: 16px; font-weight: bold;">Atenciosamente,</p>
+          <p style="font-size: 16px;">Equipe de Suporte do Sistema</p>
+      </div>
+    `;
 
-    // await sendEmail(userEmail, emailSubject, emailBody);
+    await sendEmail(userEmail, emailSubject, emailBody);
 
     return { 
         status: 200,
