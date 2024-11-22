c81c5da8127b5924094811f4ab18fe0a1f1189ab
Lucas Fidelis <lucasm241301@gmail.com>
2024-11-21 22:01:56 -0300

    Ajuste para gerar build em ambos os formatos, esm e cjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/server.ts"], // Ponto de entrada
   outDir: "dist", // Diretório de saída
-  format: ["esm"], // Formatos de saída (CommonJS e ES Module)
+  format: ['cjs', 'esm'], // Formatos de saída (CommonJS e ES Module)
   clean: true, // Limpar a pasta 'dist' antes de gerar os arquivos
   dts: true, // Gera arquivos de tipos .d.ts
 });
