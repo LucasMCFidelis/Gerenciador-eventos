d6ef79b957ec53e760989e01e144ac185103bff1
Lucas Fidelis <lucasm241301@gmail.com>
2024-11-23 18:01:34 -0300

    Ajustes para adicionar validações na criação dos admin atraves do script de seed, e ajustes na segurança das senhas

--- src/utils/db/seed.ts
+++ src/utils/db/seed.ts
@@ -1,65 +1,103 @@
-import { hashPassword } from "../security/hashPassword.js"
-import { prisma } from "./prisma.js"
+import { schemaCadastre } from "../../schemas/schemaUserCadastre.js";
+import { schemaUserPassword } from "../../schemas/schemaUserPassword.js";
+import { hashPassword } from "../security/hashPassword.js";
+import { getRoleByName } from "./getRoleByName.js";
+import { prisma } from "./prisma.js";
 
 async function seedRoles() {
-  const existingRoles = await prisma.role.findFirst()
+  const existingRoles = await prisma.role.findFirst();
   if (existingRoles) {
-    console.log('Roles já foram criados.')
-    return
+    console.log("Roles já foram criados.");
+    return;
   }
-  
+
   await prisma.role.createMany({
     data: [
-      { roleName: 'Admin', roleDescription: 'Acesso total ao sistema' },
-      { roleName: 'User', roleDescription: 'Pode visualizar e se inscrever em eventos' },
+      { roleName: "Admin", roleDescription: "Acesso total ao sistema" },
+      {
+        roleName: "User",
+        roleDescription: "Pode visualizar e se inscrever em eventos",
+      },
     ],
-  })
-  console.log('Papéis criados com sucesso!')
+  });
+  console.log("Papéis criados com sucesso!");
 }
 
 async function seedAdmins() {
   // Verificar se os dados já existem
-  const existingUser = await prisma.user.findFirst()
+  const existingUser = await prisma.user.findFirst();
   if (existingUser) {
-    console.log('Seed já foi executado.')
-    return
+    console.log("Seed já foi executado.");
+    return;
   }
-  
-  await prisma.user.createMany({
-    data: [
-      {
-        firstName: 'John', 
-        lastName: 'Doe acme', 
-        email: 'john.doe@example.com',
-        phoneNumber: '5551234567',
-        password: await hashPassword('@J0hnD03#'),
-        roleId: 1
-      },
-      {
-        firstName: 'Carlos', 
-        lastName: 'Pedro Araújo', 
-        email: 'carlos@example.com',
-        phoneNumber: '5588874567',
-        password: await hashPassword('@JMKIoowD03#'),
-        roleId: 1
-      }
-    ]
-  })
-  console.log('Admins criados com sucesso!')
+
+  const password = process.env.ADMIN_PASSWORD || "Admin33@!e";
+  const hashedPassword = await hashPassword(password);
+
+  const admins = [
+    {
+      firstName: "John",
+      lastName: "Doe acme",
+      email: "john.doe@example.com",
+      phoneNumber: "5551234567",
+      password,
+    },
+    {
+      firstName: "Carlos",
+      lastName: "Pedro Araújo",
+      email: "carlos@example.com",
+      phoneNumber: "5588874567",
+      password,
+    },
+  ];
+
+  const roleResponse = await getRoleByName("Admin");
+
+  for (const admin of admins) {
+    try {
+      // Validação dos dados com schemas
+      await schemaCadastre.concat(schemaUserPassword).validateAsync({
+        firstName: admin.firstName,
+        lastName: admin.lastName,
+        email: admin.email,
+        phoneNumber: admin.phoneNumber,
+        password: admin.password,
+      });
+
+      // Criptografar a senha do usuário
+
+      await prisma.user.create({
+        data: {
+          firstName: admin.firstName,
+          lastName: admin.lastName,
+          email: admin.email,
+          phoneNumber: admin.phoneNumber,
+          password: hashedPassword,
+          roleId: roleResponse.data?.roleId,
+        },
+      });
+      
+      console.log(`${admin.firstName} criado`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log("Admins criados com sucesso!");
 }
 
 async function main() {
-  await seedRoles()
-  await seedAdmins()
+  await seedRoles();
+  await seedAdmins();
 
-  console.log('Seed executado com sucesso.')
+  console.log("Seed executado com sucesso.");
 }
 
 main()
-  .catch(e => {
-    console.error(e)
-    process.exit(1)
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
