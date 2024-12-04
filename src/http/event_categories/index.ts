6700b029ed1de8d0bd0544139b807b186e165b10
Lucas Fidelis <lucasm241301@gmail.com>
2024-12-04 08:33:04 -0300

    Adiciona rota de atualização de categoria de evento

--- src/http/event_categories/index.ts
+++ src/http/event_categories/index.ts
@@ -3,10 +3,12 @@ import { createEventCategoryRoute } from "./createEventCategory.js";
 import { listEventCategoriesRoute } from "./listEventCategories.js";
 import { deleteEventCategoryRoute } from "./deleteEventCategoryRoute.js";
 import { getEventCategoryRoute } from "./getEventCategoryRoute.js";
+import { UpdateEventCategoryRoute } from "./updateEventCategoryRoute.js";
 
 export async function eventCategoryRoutes(fastify:FastifyInstance) {
     fastify.register(createEventCategoryRoute)
     fastify.register(listEventCategoriesRoute)
     fastify.register(deleteEventCategoryRoute)
     fastify.register(getEventCategoryRoute)
+    fastify.register(UpdateEventCategoryRoute)
 }
---
+++ src/http/event_categories/updateEventCategoryRoute.ts
@@ -0,0 +1,59 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "../../utils/db/prisma.js"
+import { handleError } from "../../utils/handlers/handleError.js"
+import { checkRole } from "../../utils/security/checkRole.js"
+import { ErrorResponse } from "../../types/errorResponseType.js"
+import { EventCategory } from "@prisma/client"
+import { schemaEventCategoryUpdate } from "../../schemas/schemaEventCategoryUpdate.js"
+import { checkExistingEventCategory } from "../../utils/validators/checkExistingEventCategory.js"
+
+export async function UpdateEventCategoryRoute(fastify: FastifyInstance) {
+    fastify.put<{
+        Params: { id: string },
+        Body: Partial<EventCategory>
+    }>('/eventos-organizadores/:id',  {
+        onRequest: [fastify.authenticate, await checkRole('Admin')]
+    }, async (request, reply) => {
+        try {
+            const { categoryName, categoryDescription } = request.body
+            const categoryId = request.params.id    
+            
+            const user = request.user
+
+            if (!user.userId) {
+                const errorValue: ErrorResponse = "Erro de validação"
+                return reply.status(400).send({ 
+                    error: errorValue,
+                    message: "userId é obrigatório" 
+                })
+            }
+
+            await schemaEventCategoryUpdate.validateAsync({ 
+                categoryName,
+                categoryDescription
+            })
+
+            const checkResponse = await checkExistingEventCategory(categoryId)
+            if (!checkResponse.categoryExisting) {
+                return reply.status(checkResponse.status).send({
+                    error: checkResponse.error,
+                    message: checkResponse.message
+                })
+            }
+
+            const updatedEventCategory = await prisma.eventCategory.update({
+                data: {
+                    ...(categoryName && { categoryName }),
+                    ...(categoryDescription && { categoryDescription }),
+                },
+                where: {
+                    categoryId
+                }
+            })
+
+            return reply.status(200).send(updatedEventCategory)
+        } catch (error) {
+            return handleError(error, reply)
+        }
+    })
+}
